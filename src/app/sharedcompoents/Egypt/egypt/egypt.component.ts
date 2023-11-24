2cbb214ffbd475483731ad5d91a42c177ef2fcfc
rania-ibrhem30 <raniamansour967@gmail.com>
2023-11-24 13:58:53 +0200

    geo loation

--- src/app/sharedcompoents/Egypt/egypt/egypt.component.ts
+++ src/app/sharedcompoents/Egypt/egypt/egypt.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiService } from '../../api.service';
+declare const L: any;
+
 
 @Component({
   selector: 'app-egypt',
@@ -15,6 +17,7 @@ export class EgyptComponent {
   egypt:any[]=[];
   item: any;
   
+
   getEg(){
     this.api.getnewsEg().subscribe(
       (res: any) => {
@@ -32,10 +35,71 @@ export class EgyptComponent {
   handleImageError() {
     this.item.urlToImage = '../../../assets/WhatsApp Image 2023-11-20 at 3.45.55 PM.jpeg'; // Replace with the URL of your placeholder image
   }
-  ngOnInit(): void{
-    this.getEg()
+  // ngOnInit(): void{
+  //   this.getEg()
     
+  // }
+  
+  ngOnInit() {
+    if (!navigator.geolocation) {
+      console.log('location is not supported');
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+      const coords = position.coords;
+      const latLong = [coords.latitude, coords.longitude];
+      console.log(
+        `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
+      );
+      let mymap = L.map('map').setView(latLong, 13);
+
+      L.tileLayer(
+        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3VicmF0MDA3IiwiYSI6ImNrYjNyMjJxYjBibnIyem55d2NhcTdzM2IifQ.-NnMzrAAlykYciP4RP9zYQ',
+        {
+          attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          maxZoom: 18,
+          id: 'mapbox/streets-v11',
+          tileSize: 512,
+          zoomOffset: -1,
+          accessToken: 'your.mapbox.access.token',
+        }
+      ).addTo(mymap);
+
+      let marker = L.marker(latLong).addTo(mymap);
+
+      marker.bindPopup('<b>Hi</b>').openPopup();
+
+      let popup = L.popup()
+        .setLatLng(latLong)
+        .setContent('I am Subrat')
+        .openOn(mymap);
+    });
+    this.watchPosition();
   }
 
-
+  watchPosition() {
+    let desLat = 0;
+    let desLon = 0;
+    let id = navigator.geolocation.watchPosition(
+      (position) => {
+        console.log(
+          `lat: ${position.coords.latitude}, lon: ${position.coords.longitude}`
+        );
+        if (position.coords.latitude === desLat) {
+          navigator.geolocation.clearWatch(id);
+        }
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      }
+    );
+  }
 }
+
+
+
